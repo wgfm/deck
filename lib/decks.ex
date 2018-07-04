e558d19d87c7cd580271faa29ea5e69009d6befc
Werner Hofstra <wernerhofstra@gmail.com>
2018-07-04 18:44:22 +0200

    Use only one standard_52 function

--- lib/decks.ex
+++ lib/decks.ex
@@ -1,30 +1,25 @@
 # Decks provides convenience functions to create different
 # kinds of decks, such as a standard 52 deck, a deck with Jokers
 # or a Pickard deck
-defmodule Decks
-  @suits = [:spades, :hearts, :clubs, :diamonds]
-  @values = [:ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king]
+defmodule Decks do
+  @suits [:spades, :hearts, :clubs, :diamonds]
+  @values [:ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, :jack, :queen, :king]
 
-  def standard_52(should_shuffle \\ true) do
+  def standard_52(opts \\ []) do
     cards = for suit <- @suits, value <- @values, do: %Card{suit: suit, value: value}
-    %Deck{ cards: cards }
-    |> maybe_shuffle(should_shuffle)
-  end
-
-  def standard_52(num_jokers, should_shuffle \\ true) do
-    deck = standard_52(false)
+    deck = %Deck{ cards: cards }
 
     Cards.joker
-    |> List.duplicate(num_jokers)
+    |> List.duplicate(opts[:jokers] || 0)
     |> Enum.reduce(deck, fn (deck, card) -> Deck.add(deck, card) end)
-    |> maybe_shuffle(should_shuffle)
+    |> maybe_shuffle(opts[:shuffle])
   end
 
   defp maybe_shuffle(deck, should_shuffle=true) do
     Deck.shuffle(deck)
   end
 
-  defp maybe_shuffle(deck, should_shuffle=false) do
+  defp maybe_shuffle(deck, _should_shuffle) do
     deck
   end
 end
